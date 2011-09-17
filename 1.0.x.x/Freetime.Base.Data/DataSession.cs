fdc78b142d67076b97baa9b72d3485989b5ab1b6
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-09-17 14:17:27 +0800

    Edited Freetime.Base.Data/DataSession.cs

--- 1.0.x.x/Freetime.Base.Data/DataSession.cs
+++ 1.0.x.x/Freetime.Base.Data/DataSession.cs
@@ -18,7 +18,7 @@ namespace Freetime.Base.Data
             }
         }
 
-        private FreetimeUser CurrentUser { get; set;}
+        protected FreetimeUser CurrentUser { get; set;}
 
         private static ISession GetDefaultSession()
         {
