b341f17db9c4d48585c5faa7e27824056e83e6b1
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-09-24 11:49:54 +0800

    Added Unit Tests

---
+++ 1.0.x.x/Unit.Testing/Test.Freetime.Base.Data/LocalizationSessionTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Protected;
+using Freetime.Base.Data;
+using Freetime.Base.Data.Entities;
+using Anito.Data;
+
+namespace Test.Freetime.Base.Data
+{
+    /// <summary>
+    /// Unit Test for Freetime.Base.Data.LocalizationSession
+    /// </summary>
+    /// <Assembly>Freetime.Base.Data</Assembly>
+    /// <Class>Freetime.Base.Data.LocalizationSession</Class>
+    [TestClass]
+    public class LocalizationSessionTest
+    {        
+
+        [TestMethod]
+        public void GetLanguage()
+        {
+            Mock<LocalizationSession> localizationSession = new Mock<LocalizationSession> { CallBase = true };
+
+            Mock<ISession> anitoSession = new Mock<ISession> { CallBase= true };
+            
+            string languageCode = "English - United States";
+
+            Language language = new Language
+            {
+                LanguageCode = languageCode,
+                ISOStandardCode = "en-US",
+                IsActive = true
+            };
+
+            anitoSession.Setup(x => x.GetT<Language>(It.IsAny<Expression<Func<Language, bool>>>())).Returns(language);
+
+            localizationSession.Protected().Setup<ISession>("CurrentSession").Returns(anitoSession.Object);
+                      
+            var expected = localizationSession.Object.GetLanguage(languageCode);
+
+            Assert.AreEqual(language, expected);
+        }
+
+        [TestMethod]
+        public void GetLanguageThrowsArgumentNullException()
+        {
+            LocalizationSession target = new LocalizationSession();
+
+            Exception exception = null;
+
+            try
+            {
+                target.GetLanguage(null);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            Assert.IsNotNull(exception);
+
+            Assert.AreEqual<Type>(
+               typeof(ArgumentNullException), exception.GetType());
+
+        }
+
+    }
+}
