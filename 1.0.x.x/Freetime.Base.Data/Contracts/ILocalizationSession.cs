c9dde1e4a5684b6e13a4fce14c6dcc4773ce2b25
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-09-26 18:23:19 +0800

    Added LocalizationSession

---
+++ 1.0.x.x/Freetime.Base.Data/Contracts/ILocalizationSession.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ServiceModel;
+using Freetime.Base.Data.Entities;
+
+namespace Freetime.Base.Data.Contracts
+{
+    [ServiceContract]
+    public interface ILocalizationSession : IDataSession
+    {
+        [OperationContract]
+        Language GetLanguage(string languageCode);
+    }
+}
