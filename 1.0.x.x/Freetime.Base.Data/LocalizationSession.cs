8a789a1324ba3d54247516991f942b01bc00aa37
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-09-26 18:23:57 +0800

    Added LocalizationSession.cs

---
+++ 1.0.x.x/Freetime.Base.Data/LocalizationSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Freetime.Base.Data.Contracts;
+using Freetime.Base.Data.Entities;
+
+namespace Freetime.Base.Data
+{
+    public class LocalizationSession : DataSession, ILocalizationSession
+    {
+        public Language GetLanguage(string languageCode)
+        {
+            if (Equals(languageCode, null))
+                throw new ArgumentNullException("languageCode");
+
+            return CurrentSession.GetT<Language>(l => l.LanguageCode == languageCode);
+        }
+    }
+}
