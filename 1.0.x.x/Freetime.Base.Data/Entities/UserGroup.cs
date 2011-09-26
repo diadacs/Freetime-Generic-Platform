d9932dbce1b9c9091f98cea532a01e3794eb172a
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-09-26 16:52:35 +0800

    Added UserGroup Entity

---
+++ 1.0.x.x/Freetime.Base.Data/Entities/UserGroup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using Freetime.Base.Data.Entities.Common;
+
+namespace Freetime.Base.Data.Entities
+{
+    [DataContract]
+    [Serializable]
+    [XmlRoot("UserGroup",
+        Namespace = "http://www.freeG-businessplatform.com",
+        IsNullable = true)]
+    public class UserGroup : AuditableEntity
+    {      
+
+        [DataMember]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [DataMember]
+        [XmlElement("Description")]
+        public string Description { get; set; }
+
+        [DataMember]
+        [XmlElement("IsActive")]
+        public bool IsActive { get; set; }
+
+    }
+}
