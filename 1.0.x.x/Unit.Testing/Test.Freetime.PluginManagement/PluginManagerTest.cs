6096367dfd6462e078841272438292c2ac9725c8
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-09-27 11:19:39 +0800

    Added Unit Test

---
+++ 1.0.x.x/Unit.Testing/Test.Freetime.PluginManagement/PluginManagerTest.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using Freetime.PluginManagement;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Freetime.PluginManagement
+{
+    /// <summary>
+    /// Unit Test for Freetime.PluginManagement.PluginManager
+    /// </summary>
+    /// <Assembly>Freetime.PluginManagement.</Assembly>
+    /// <Class>Freetime.PluginManagement.PluginManager</Class>
+    [TestClass]
+    public class PluginManagerTest
+    {
+        /// <summary>
+        /// Expected : PluginManager Not Null
+        /// </summary>
+        [TestMethod]
+        public void PluginManagerNotNullTest()
+        {
+            var pluginManager = new Mock<IPluginManager>();
+            PluginManager.SetPluginManager(pluginManager.Object);
+            Assert.IsTrue(PluginManager.Current != null);
+        }
+    }
+}
