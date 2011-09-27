6e6490e564b8de5ae72425fc7aa1bbab5416f290
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-09-27 10:14:40 +0800

    Added Unit Tests

---
+++ 1.0.x.x/Unit.Testing/Test.Freetime.Authentication/FreetimeUserTest.cs
@@ -0,0 +1,25 @@
+﻿using Freetime.Authentication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Freetime.Authentication
+{
+    /// <summary>
+    /// Unit Test for Freetime.Base.Framework.Crypto
+    /// </summary>
+    /// <Assembly>Freetime.Base.Framework</Assembly>
+    /// <Class>Freetime.Base.Framework.Crypto</Class>
+    [TestClass]
+    public class FreetimeUserTest
+    {
+
+        /// <summary>
+        /// Expected : Initialized FreetimeUser
+        /// </summary>
+        [TestMethod]
+        public void InitiateFreetimeUserTest()
+        {
+            var user = new FreetimeUser(1, 1, "Test User", true, "DefaultTheme");
+            Assert.IsNotNull(user);
+        }
+    }
+}
---
+++ 1.0.x.x/Unit.Testing/Test.Freetime.Base.Framework/CryptoTest.cs
@@ -0,0 +1,25 @@
+﻿using Freetime.Base.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Freetime.Base.Framework
+{
+    /// <summary>
+    /// Unit Test for Freetime.Base.Framework.Crypto
+    /// </summary>
+    /// <Assembly>Freetime.Base.Framework</Assembly>
+    /// <Class>Freetime.Base.Framework.Crypto</Class>
+    [TestClass]
+    public class CryptoTest
+    {
+
+        /// <summary>
+        /// Expected : Crypto should not be null
+        /// </summary>
+        [TestMethod]
+        public void CryptoNullTest()
+        {
+            var provider = Crypto.Md5CryptoServiceProvider;
+            Assert.IsNotNull(provider);
+        }
+    }
+}
