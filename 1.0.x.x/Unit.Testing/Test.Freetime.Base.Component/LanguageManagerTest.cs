fdbabe2886089f571bdee17b34ce9bd830e02eac
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-09-29 10:37:01 +0800

    Added Freetime.Base.Component Unit Test

---
+++ 1.0.x.x/Unit.Testing/Test.Freetime.Base.Component/LanguageManagerTest.cs
@@ -0,0 +1,24 @@
+﻿using Freetime.Base.Component;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Freetime.Base.Component
+{
+    /// <summary>
+    /// Unit Test for Freetime.Base.Component.LanguageManager
+    /// </summary>
+    /// <Assembly>Freetime.Base.Component</Assembly>
+    /// <Class>Freetime.Base.Component.LanguageManager</Class>
+    [TestClass]
+    public class LanguageManagerTest
+    {
+
+        /// <summary>
+        /// Expected : Current Lanaguae Manager Not Null
+        /// </summary>
+        [TestMethod]
+        public void LanguageManagerCurrentNotNullTest()
+        {
+            Assert.IsNotNull(LanguageManager.Current);
+        }
+    }
+}
